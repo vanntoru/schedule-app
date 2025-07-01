9f5ae8d1e77053a96b52f44d5a9ff19b1463d276
vanntoru <154296227+vanntoru@users.noreply.github.com>
2025-07-01 13:28:06 +0900

    Handle ISO8601 calendar date

--- schedule_app/api/calendar.py
+++ schedule_app/api/calendar.py
@@ -20,10 +20,22 @@ def get_calendar() -> tuple[list[dict], int] | tuple[dict, int]:
     if not date_str:
         abort(400, description="date parameter required")
 
+    # If '+' was decoded to ' ', restore it
+    if " " in date_str and "+" not in date_str:
+        date_str = date_str.replace(" ", "+")
+
+    if date_str.endswith("Z"):
+        date_str = date_str[:-1] + "+00:00"
+
     try:
-        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+        date_obj = datetime.fromisoformat(date_str)
     except ValueError:
         abort(400, description="invalid date format")
+
+    if date_obj.tzinfo is None:
+        date_obj = date_obj.replace(tzinfo=timezone.utc)
+    else:
+        date_obj = date_obj.astimezone(timezone.utc)
 
     client: GoogleClient = current_app.extensions["gclient"]
 
